7eadd4fc7b649c7df8a4d8fe347844128661fa87
HyeonseopJeong <gustjq1101@naver.com>
2021-01-20 17:14:42 +0900

    print mainpage images as binary

--- mainpage.php
+++ mainpage.php
@@ -1,12 +1,15 @@
 <?php
-    require_once("./lib/mysql_connect.php");
+    session_start();
 
-    $query = "SELECT * FROM post LEFT JOIN user ON post.writer_id=user.id";
+    require_once("./lib/mysql_connect.php");
+    require_once("./api/google_oauth2/client_setting.php");
+
+    $query = "SELECT * FROM posting LEFT JOIN user ON posting.writer_id=user.id";
     $result = mysqli_query($conn, $query);
     $posts = array();
     while($row = mysqli_fetch_array($result)) {
         $post = array(
-            'image_path' => $row['image_path'],
+            'image' => 'data: image/;base64,' .  base64_encode($row['image']),
             'title' => htmlspecialchars($row['title']),
             'period' => $row['begin_date'] . ' ~ ' . $row['end_date'],
             'writer' => $row['lastname'] . ' ' . $row['firstname'],
@@ -16,11 +19,30 @@
             $post['title'] = substr($post['title'], 0, 18) . ' ...'; 
         }
         array_push($posts, $post);
-        //var_dump($post);
-        //echo "<br><br>";
-    }
-    //die();
 
+        //print_r($post);
+        //die("<br>@<br>");
+    }
+
+    $login_message = "";
+    if(!isset($_SESSION['user_id'])) {
+        $login_message = "<a href=\"" . $callback_url . "\"> 로그인 </a>";
+    }
+    else {
+        $sql = "SELECT firstname, lastname FROM user WHERE id = " . $_SESSION['user_id'];
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result);
+        if($result === false) {
+            die("fail to get login");
+        }
+        if(mysqli_num_rows($result) === 0) {
+            unset($_SESSION['user_id']);
+            header("Location: " . $callback_uri);
+        }
+        else {
+            $login_message = "Hello, " . $row['firstname'] . ' ' . $row['lastname'] . ' ' . '<a href="' . $callback_url . '?logout=1">logout<a>';
+        }
+    }
 
 
 
@@ -39,12 +61,13 @@
 </head>
 <body>
 
+    <header> <?=$login_message?> </header>
     <div class="posts-container">
         <?php
         foreach ($posts as $post){
         ?>
         <div class="post">
-            <div class="img-container"><img src="<?=$post['image_path']?>" alt=""></div>
+            <div class="img-container"><img src="<?=$post['image']?>" alt=""></div>
             <div class="post-info">
                 <div class="title"><?=$post['title']?></div>
                 <div class="period"><?=$post['period']?></div>
