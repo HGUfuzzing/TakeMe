593ddc7cb6eed226dfb0dfe215e2f76858a96663
HyeonseopJeong <gustjq1101@naver.com>
2021-01-22 17:01:38 +0900

    add mysql filter to content text

--- create_news_process.php
+++ create_news_process.php
@@ -9,11 +9,18 @@
     require_once($_SERVER['DOCUMENT_ROOT'] . '/api/google_oauth2/client_setting.php');
     require_once($_SERVER['DOCUMENT_ROOT'] . '/lib/mysql_connect.php');
 
+
+    $filtered = array (
+        'post_id' => $_POST['post_id'],
+        'user_id' => $_SESSION['user_id'],
+        'content' => mysqli_real_escape_string($conn, $_POST['content'])
+    );
+
     $sql = 'INSERT INTO news (post_id, user_id, content) '
     . 'VALUES('
-    . '"' . $_POST['post_id'] . '", ' 
-    . '"' . $_SESSION['user_id'] . '", '
-    . '"' . $_POST['content'] . '" '
+    . '"' . $filtered['post_id'] . '", ' 
+    . '"' . $filtered['user_id'] . '", '
+    . '"' . $filtered['content'] . '" '
     . ')';
 
     $result = mysqli_query($conn, $sql);
