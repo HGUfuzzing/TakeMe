27b3e092ccd424ce0384dd8d4eb4bdc7f5122002
LeeHyunji <leehyunji0715@gmail.com>
2021-02-22 20:10:49 +0900

    edit search input desing in main page

--- views/main.view.php
+++ views/main.view.php
@@ -3,10 +3,12 @@
 <link rel="stylesheet" href="/public/css/main.css">
 <script src="/public/js/main.js" defer></script>
 
-<div class="search-container">
-    @
-    <input type="text" name="text" placeholder="이동할 이벤트의 키워드를 입력하세요.">
-    <button>GO</button>
+<div class="search-wrapper">
+    <div class="search-container">
+        <i class="fas fa-at"></i>
+        <input type="text" name="text" placeholder="이동할 이벤트의 키워드를 입력하세요.">
+        <button style="display:none"></button>
+    </div>
 </div>
 
 <div class="posts-container">
