d37c414195d0382afc69eec10c5695cd43ded868
LeeHyunji <leehyunji0715@gmail.com>
2021-02-22 20:36:58 +0900

    edit label in write page

--- views/write.view.php
+++ views/write.view.php
@@ -21,31 +21,31 @@
                     <label id="keyword">http://gohandong.cafe24.com/@<?=$post->link_keyword?></label>
                 <?php else: ?>
                     <label class="post-label">
-                        <span class="highlight">*</span> 포스트 키워드
-                    </label>
-                    <br><input type="text" id="link-keyword-input" name="link-keyword-input" placeholder="한글, 영어, 숫자, - 가능" required>
+                        <span class="highlight">*</span> 키워드
+                    </label><br>
+                    @<input type="text" id="link-keyword-input" name="link-keyword-input" placeholder="한글, 영어, 숫자, - 가능" required>
                 <?php endif; ?>
 
                 <div class="keyword_check_msg"></div>
             </div>
             <div class = "input-block">
-                <label class="post-label">링크 주소</label><br/>
+                <label class="post-label">타겟 링크</label><br/>
                 <input type="text" name="url-input" value="<?=$post->link?>" placeholder="Zoom, Youtube 링크 등..">
             </div>
             <div class="input-block">
-                <label class="post-label">행사 기간</label>
+                <label class="post-label">유효 기간</label>
                 <input type="radio" id="set-eventdate" name="eventdate" onclick="showDate()" <?php if($post->begin_date !== null) echo "checked"?>> <label class="option">설정</label>
                 <input type="radio" id="unset-eventdate" name="eventdate" onclick="showNone()" <?php if($post->begin_date === null) echo "checked"?>> <label class="option">미설정</label>
                 
                 <div id="eventdatetime-container">
-                    <label class="post-label"><span class="highlight">*</span> 행사 시작</label>
+                    <label class="post-label"><span class="highlight">*</span> 시작</label>
                     <input type="date" class="date-input" id="begin_date" name="begin_date" value="<?=$post->e_begin_date?>" required> <br><br>
-                    <label class="post-label"><span class="highlight">*</span> 행사 종료</label>
+                    <label class="post-label"><span class="highlight">*</span> 종료</label>
                     <input type="date" class="date-input" id="end_date" name="end_date" value="<?=$post->e_end_date?>" required>
                 </div>
             </div>
             <div class="input-block" id="file-upload">
-                <label class="post-label">포스터 업로드 </label>
+                <label class="post-label">이미지 업로드 </label>
                 <input type='file' id='file-input' name='file-input' accept="image/*">
             </div>
         </div>
