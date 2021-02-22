a5e3972e347d6f29d6bf22b6b1eca20ee78876f7
HyeonseopJeong <gustjq1101@naver.com>
2021-02-22 22:49:14 +0900

    gather main controllers' functions in main.lib.php

--- controllers/ajax/search-keyword.php
+++ controllers/ajax/search-keyword.php
@@ -1,7 +1,7 @@
 <?php
+require 'controllers/lib/main.lib.php';
 
 $keyword = $_GET['keyword'];
-
 
 $sql = ''
 . 'SELECT '
@@ -29,57 +29,3 @@ foreach($posts as $post)
 }
 
 require 'views/main.pagination.view.php';
-
-
-
-
-
-
-
-
-
-function get_event_state($begin_date, $end_date){
-    $cur = date_create(date("Y-m-d"));
-    $begin = date_create($begin_date);
-    $end = date_create($end_date);
-
-    $diff_started = date_diff($begin, $cur);
-    $diff_ended = date_diff($end, $cur);
-
-    $has_started = ($diff_started->format('%R%a') >= 0)? true : false;
-    $has_ended = ($diff_ended->format('%R%a') >= 0)? true: false;
-
-    if($has_started && $has_ended){
-        return ['status' => 'end'];
-    }
-    else if($has_started && !$has_ended){
-        return ['status' => 'running', 'remain_date' => abs($diff_ended->format('%R%a'))];
-    }
-    else if(!$has_started && !$has_ended){
-        return ['status' => 'upcomming', 'remain_date' => $diff_started->format('%R%a')];
-    }
-    
-    return '<span>잘못된 날짜</span>';
-}
-
-
-function print_star_icon($post_id) {
-    global $conn;
-
-    if(!isset($_SESSION['user_id'])) {
-        return;
-    }
-
-    $sql = ''
-    . 'SELECT id '
-    . 'FROM favorite '
-    . 'WHERE post_id = ' . $post_id . ' and user_id = ' . $_SESSION['user_id'];
-
-    $count = App::get('database')->rowCount($sql);
-
-    if($count === 0) {
-        return false;
-    }
-
-    return true;
-}
