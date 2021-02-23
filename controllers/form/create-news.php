f3b7abf953514475332b0ddfd4996334c6c5ca03
HyeonseopJeong <gustjq1101@naver.com>
2021-02-23 15:58:52 +0900

    fix: keyword in email title is omitted

--- controllers/form/create-news.php
+++ controllers/form/create-news.php
@@ -40,16 +40,7 @@ if(count($email_addresses) === 0){
 
 require_once 'controllers/lib/mail.php';
 
-$short_post_keyword = $post_keyword;
-$encodes = array('ASCII', 'UTF-8', 'EUC-KR');
-$keyword_encode = mb_detect_encoding($post_keyword, $encodes); 
-
-if(mb_strlen($short_post_keyword, $keyword_encode) > 7) {
-    $short_post_keyword = mb_substr($short_post_keyword, 0, 7, $title_encode);
-    $short_post_keyword .= '...';
-}
-
-$subject = '즐겨찾기 한 게시물 "@' . $short_post_keyword . '" 에 news가 생겼습니다.';
+$subject = '즐겨찾기 한 게시물 "@' . $post_keyword . '" 에 news가 생겼습니다.';
 $content = str_replace(array('\\r\\n', '\\r\\n', '\\r', '\\n'),'',$news['content']);
 $body = ''
 . '즐겨찾기 한 게시물 "@' . $post_keyword . '" 에 news가 생겼습니다. <br><br>'
